fe1c077861243f4437d7f0d9bcab8d467656793b
vthokalak <45837615+vthokalak@users.noreply.github.com>
2018-12-16 21:37:45 -0500

    Revert "New Branch Santhosh Checkin"

--- Pep/Web/App_Start/BundleConfig.cs
+++ Pep/Web/App_Start/BundleConfig.cs
@@ -9,44 +9,22 @@ namespace Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                             "~/Scripts/Lib/jquery-3.2.1.min.js",
-                               "~/Scripts/Lib/jquery.validate.min.js",
-                               "~/Scripts/Lib/jquery.validate.unobtrusive.min.js",
-                               "~/Scripts/Lib/jquery.validate-vsdoc.min.js",
-                            "~/Scripts/Lib/jquery.validate.unobtrusive.min.js"));
-            bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                             "~/Scripts/Lib/jquery.signalR-2.2.3.min.js",
-                             "~/Scripts/Lib/bootstrap.min.js",
-                            "~/Scripts/scrollingnav/scrolling-nav.min.js",
-                            "~/Scripts/Lib/bootbox.min.js",
-                            "~/Scripts/Lib/htmlSanitizier.min.js",
-                            "~/Scripts/Lib/respond.min.js",
-                            "~/Scripts/Lib/moment.min.js",
-                            "~/Scripts/Lib/bootstrap-switch.min.js",
-                            "~/Scripts/Lib/jquery.toast.min.js",
-                            "~/Scripts/Lib/jquery.cookie.min.js",
-                             "~/Scripts/Lib/director.min.js",
-                            "~/Scripts/Lib/select2.min.js",
-                             "~/Scripts/Lib/handlebars-v4.0.11.min.js",
-                            "~/Scripts/Lib/handlebars.runtime-v4.0.11.min.js",
-                            "~/Scripts/Lib/URITemplate.min.js",
-                            "~/Scripts/Lib/bootstrap-datepicker.min.js",
-                             "~/Scripts/Lib/jquery-ui.min.js"));
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                       "~/Content/bootstrap.min.css",
-                       "~/Content/jquery-ui.css",
-                       "~/Content/Navbar.min.css",
-                       "~/Content/font-awesome.min.css",
-                        "~/Content/bootstrap-switch.css",
-                       "~/Content/bootstrap.min.css",
-                       "~/Content/jquery.toast.min.css",
-                       "~/Content/scrolling-nav.min.css",
-                       "~/Content/jquery.treegrid.min.css",
-                       "~/Content/Pep.css",
-                       "~/Content/site.css",
-                       "~/Content/styles.min.css",
-                       "~/Content/select2.min.css",
-                       "~/Content/bootstrap-datepicker.min.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
         }
     }
 }
